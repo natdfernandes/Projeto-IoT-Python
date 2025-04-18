e9534d4b25b20249bab7bc41aac1cc1b0469b1b4
Natalia <nataliadiasfernande@gmail.com>
2025-04-17 21:41:14 -0300

    Leitor de codigo de barras

--- leitura-qrcode/ler_qrcode.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-from pyzbar.pyzbar import decode as lerqrcode
-
-imagem = Image.open("./imagens/codigo-de-barras.jpg")
-conteudo_qrcode = lerqrcode(imagem)
-
-print(conteudo_qrcode)
-print("*" * 50)
-print(conteudo_qrcode[0].data.decode("utf-8"))
